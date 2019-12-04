ae4385fb30c87d7f1514888a2746b9981b341043
MackRoe <elaine.music@students.makeschool.com>
2019-12-03 16:32:04 -0800

    added static folder and image

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from api.views import PageList, PageDetail
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('wiki/', PageList.as_view(), name='page_list'),
     path('wiki/<str:slug>', PageDetail.as_view(), name='page_detail')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
